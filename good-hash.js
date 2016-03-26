474d57e63c4f0279d544b45b7f7b17dc28893751
Dominic Tarr <dominic.tarr@gmail.com>
2016-03-27 12:57:14 +1300

    output feeds that are broken by wrong hashes

--- good-hash.js
+++ good-hash.js
@@ -5,8 +5,9 @@ var ssbc = require('ssb-client')
 function hash1 (data) {
   return crypto.createHash('sha256')
     .update(data, 'binary')
-    .digest('base64')
+    .digest('base64')+'.sha256'
 }
+
 function hash2 (data) {
   return crypto.createHash('sha256')
     .update(new Buffer(data, 'utf8'))
@@ -23,21 +24,18 @@ ssbc(function (err, sbot) {
     pull.drain(function (msg) {
       var data = JSON.stringify(msg.value, null, 2)
       if(ssbHash(data) !== hash2(data)) {
-        if(!authors[msg.value.author])
-          authors[msg.value.author] = 1
-        authors[msg.value.author] ++//.push(msg.value.sequence)
+        if('%'+hash1(data) !== msg.key)
+          authors[msg.value.author] = msg.value.sequence
         bad ++
-        console.log(msg.key, msg.value.author, msg.value.sequence)
+        console.error(msg.key, msg.value.author, msg.value.sequence)
       }
       else
         good ++
     }, function (err) {
       console.log(JSON.stringify(authors, null, 2))
-      console.log(good, bad, bad/(good+bad))
-      console.log(Object.keys(authors).length)
+      console.error(good, bad)
       sbot.close()
     })
   )
-
 })
 
