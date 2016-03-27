cf88928d491d3a44b852a938ed576569aad57d61
Dominic Tarr <dominic.tarr@gmail.com>
2016-03-27 18:10:27 +1300

    output messages that need to be deleted

--- good-hash.js
+++ good-hash.js
@@ -19,23 +19,29 @@ var ssbHash = require('ssb-keys').hash
 ssbc(function (err, sbot) {
   var authors = {}
   var good = 0, bad = 0
+  var to_delete = []
   pull(
     sbot.createLogStream(),
     pull.drain(function (msg) {
       var data = JSON.stringify(msg.value, null, 2)
       if(ssbHash(data) !== hash2(data)) {
-        if('%'+hash1(data) !== msg.key)
+        if('%'+hash1(data) !== msg.key) {
           authors[msg.value.author] = msg.value.sequence
+          to_delete.push(msg.key)
+        }
         bad ++
-        console.error(msg.key, msg.value.author, msg.value.sequence)
       }
       else
         good ++
     }, function (err) {
       console.log(JSON.stringify(authors, null, 2))
+      console.error("delete":)
+      console.log(JSON.stringify(to_delete, null, 2))
       console.error(good, bad)
       sbot.close()
     })
   )
 })
+
+
 
